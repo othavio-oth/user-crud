c6b674fa30d14d060050d43527d26c81d5b5d305
Othavio <otaviodelimasousa@gmail.com>
2023-03-29 22:26:21 -0300

    add findUserByEmail method

--- src/main/java/com/github/othaviooth/usercrud/repository/UserRepository.java
+++ src/main/java/com/github/othaviooth/usercrud/repository/UserRepository.java
@@ -3,11 +3,14 @@ package com.github.othaviooth.usercrud.repository;
 import com.github.othaviooth.usercrud.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findUserByNameIgnoreCase(String name);
+
+    Optional<User> findUserByEmail(String email);
 
 }
